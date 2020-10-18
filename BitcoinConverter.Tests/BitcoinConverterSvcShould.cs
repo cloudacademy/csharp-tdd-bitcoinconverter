a3709395af6dcb4559585878ee49531db62a6fab
Jeremy Cook <jeremycook123@Jeremys-MacBook-Pro-2.local>
2020-10-19 12:11:36 +1300

    refactored

--- BitcoinConverter.Tests/BitcoinConverterSvcShould.cs
+++ BitcoinConverter.Tests/BitcoinConverterSvcShould.cs
@@ -7,18 +7,6 @@ namespace CloudAcademy.Bitcoin.Tests
     public class BitcoinConverterSvcShould
     {
         [Fact]
-        public void GetNZDExchangeRate()
-        {
-            //act
-            var converterSvc = new ConverterSvc();
-            var exchangeRate = converterSvc.GetExchangeRate("NZD");
-
-            //assert
-            var expected = 100;
-            Assert.Equal(expected, exchangeRate);
-        }
-
-        [Fact]
         public void GetUSDExchangeRate()
         {
             //act
@@ -26,20 +14,32 @@ namespace CloudAcademy.Bitcoin.Tests
             var exchangeRate = converterSvc.GetExchangeRate("USD");
 
             //assert
+            var expected = 100;
+            Assert.Equal(expected, exchangeRate);
+        }
+
+        [Fact]
+        public void GetGBPExchangeRate()
+        {
+            //act
+            var converterSvc = new ConverterSvc();
+            var exchangeRate = converterSvc.GetExchangeRate("GBP");
+
+            //assert
             var expected = 200;
             Assert.Equal(expected, exchangeRate);
         }
 
         [Fact]
-        public void ConvertBitcoinsToNZD()
+        public void GetEURExchangeRate()
         {
             //act
             var converterSvc = new ConverterSvc();
-            var dollars = converterSvc.ConvertBitcoins("NZD", 1);
+            var exchangeRate = converterSvc.GetExchangeRate("EUR");
 
             //assert
-            var expected = 100;
-            Assert.Equal(expected, dollars);
+            var expected = 300;
+            Assert.Equal(expected, exchangeRate);
         }
 
         [Fact]
@@ -50,8 +50,33 @@ namespace CloudAcademy.Bitcoin.Tests
             var dollars = converterSvc.ConvertBitcoins("USD", 1);
 
             //assert
+            var expected = 100;
+            Assert.Equal(expected, dollars);
+        }
+
+        [Fact]
+        public void ConvertBitcoinsToGBP()
+        {
+            //act
+            var converterSvc = new ConverterSvc();
+            var dollars = converterSvc.ConvertBitcoins("GBP", 1);
+
+            //assert
             var expected = 200;
             Assert.Equal(expected, dollars);
         }
+
+        [Fact]
+        public void ConvertBitcoinsToEUR()
+        {
+            //act
+            var converterSvc = new ConverterSvc();
+            var dollars = converterSvc.ConvertBitcoins("EUR", 1);
+
+            //assert
+            var expected = 300;
+            Assert.Equal(expected, dollars);
+        }
+
     }
 }
