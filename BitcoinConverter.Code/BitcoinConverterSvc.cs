c3f2568a2486fa1193de9b048886c2e2d26ec15f
Jeremy Cook <jeremy.cook@cloudacademy.com>
2020-10-19 13:17:15 +1300

    refactored

--- BitcoinConverter.Code/BitcoinConverterSvc.cs
+++ BitcoinConverter.Code/BitcoinConverterSvc.cs
@@ -8,9 +8,46 @@ namespace CloudAcademy.Bitcoin
 {
     public class ConverterSvc
     {
-        private const string BITCOIN_NZDUSD_URL = "https://api.coindesk.com/v1/bpi/currentprice/NZD.json";
+        private const string BITCOIN_CURRENTPRICE_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
         private HttpClient client;
+
+        /*
+        //curl -s https://api.coindesk.com/v1/bpi/currentprice.json | jq .
+        //example json response from coindesk api:
+        {
+            "time": {
+                "updated": "Oct 15, 2020 22:55:00 UTC",
+                "updatedISO": "2020-10-15T22:55:00+00:00",
+                "updateduk": "Oct 15, 2020 at 23:55 BST"
+            },
+            "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index (USD)",
+            "chartName": "Bitcoin",
+            "bpi": {
+                "USD": {
+                "code": "USD",
+                "symbol": "&#36;",
+                "rate": "11,486.5341",
+                "description": "United States Dollar",
+                "rate_float": 11486.5341
+                },
+                "GBP": {
+                "code": "GBP",
+                "symbol": "&pound;",
+                "rate": "8,900.8693",
+                "description": "British Pound Sterling",
+                "rate_float": 8900.8693
+                },
+                "EUR": {
+                "code": "EUR",
+                "symbol": "&euro;",
+                "rate": "9,809.3278",
+                "description": "Euro",
+                "rate_float": 9809.3278
+                }
+            }
+        }
+        */
 
         public ConverterSvc()
         {
@@ -22,40 +59,44 @@ namespace CloudAcademy.Bitcoin
             this.client = httpClient;
         }
 
-        public async Task<double> GetExchangeRate(string currency)
+        public enum Currency 
+        {
+            USD,
+            GBP,
+            EUR        
+        }
+
+        public async Task<double> GetExchangeRate(Currency currency)
         {
             double rate = 0;
 
             try
             {
-                var response = await this.client.GetStringAsync(BITCOIN_NZDUSD_URL);
+                var response = await this.client.GetStringAsync(BITCOIN_CURRENTPRICE_URL);
                 var jsonDoc = JsonDocument.Parse(Encoding.ASCII.GetBytes(response));
 
-                if(currency.Equals("NZD"))
-                {
-                    var nzdRate = jsonDoc.RootElement.GetProperty("bpi").GetProperty("NZD").GetProperty("rate");
+                var rateStr = jsonDoc.RootElement.GetProperty("bpi").GetProperty(currency.ToString()).GetProperty("rate");
 
-                    rate = Double.Parse(nzdRate.GetString());
-                }
-                else if (currency.Equals("USD"))
-                {
-                    var usdRate = jsonDoc.RootElement.GetProperty("bpi").GetProperty("USD").GetProperty("rate");
-
-                    rate = Double.Parse(usdRate.GetString());
-                }
+                rate = Double.Parse(rateStr.GetString());
             }
             catch
             {
                 rate = -1;
             }
 
-            return rate;
+            return Math.Round(rate, 4);
         }
 
-        public async Task<double> ConvertBitcoins(string currency, int coins)
+        public async Task<double> ConvertBitcoins(Currency currency, double coins)
         {
-            double exchangeRate = await GetExchangeRate(currency);
             double dollars = 0;
+            
+            if (coins < 0)
+            {
+                throw new ArgumentException("Number of coins must not be less than zero"); 
+            }
+
+            double exchangeRate = await GetExchangeRate(currency);
 
             if (exchangeRate >= 0)
             {
@@ -66,7 +107,7 @@ namespace CloudAcademy.Bitcoin
                 dollars = -1;
             }
 
-            return dollars;
+            return Math.Round(dollars, 4);
         }
 
     }
