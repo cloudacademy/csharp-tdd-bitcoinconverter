de4df7a2c2b26f4b701983c79eaec4579b62e826
Jeremy Cook <jeremy.cook@cloudacademy.com>
2020-11-23 11:24:40 +1300

    updates

---
+++ BitcoinConverter.Code/BitcoinConverterSvc.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+
+namespace CloudAcademy.Bitcoin
+{
+    public class ConverterSvc
+    {
+        private const string BITCOIN_CURRENTPRICE_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+        /*
+        //curl -s https://api.coindesk.com/v1/bpi/currentprice.json | jq .
+        //example json response from coindesk api:
+        {
+            "time": {
+                "updated": "Oct 15, 2020 22:55:00 UTC",
+                "updatedISO": "2020-10-15T22:55:00+00:00",
+                "updateduk": "Oct 15, 2020 at 23:55 BST"
+            },
+            "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index (USD)",
+            "chartName": "Bitcoin",
+            "bpi": {
+                "USD": {
+                    "code": "USD",
+                    "symbol": "&#36;",
+                    "rate": "11,486.5341",
+                    "description": "United States Dollar",
+                    "rate_float": 11486.5341
+                },
+                "GBP": {
+                    "code": "GBP",
+                    "symbol": "&pound;",
+                    "rate": "8,900.8693",
+                    "description": "British Pound Sterling",
+                    "rate_float": 8900.8693
+                },
+                "EUR": {
+                    "code": "EUR",
+                    "symbol": "&euro;",
+                    "rate": "9,809.3278",
+                    "description": "Euro",
+                    "rate_float": 9809.3278
+                }
+            }
+        }
+        */
+
+        public ConverterSvc()
+        {
+        }
+
+        public async Task<double> GetExchangeRate(string currency)
+        {
+            var response = await new HttpClient().GetStringAsync(BITCOIN_CURRENTPRICE_URL);
+
+            if(currency.Equals("USD"))
+            {
+                var jsonDoc = JsonDocument.Parse(Encoding.ASCII.GetBytes(response));
+                var rate = jsonDoc.RootElement.GetProperty("bpi").GetProperty(currency).GetProperty("rate");
+
+                return Double.Parse(rate.GetString());
+            }
+            else if (currency.Equals("GBP"))
+            {
+                var jsonDoc = JsonDocument.Parse(Encoding.ASCII.GetBytes(response));
+                var rate = jsonDoc.RootElement.GetProperty("bpi").GetProperty(currency).GetProperty("rate");
+
+                return Double.Parse(rate.GetString());
+            }
+            else if (currency.Equals("EUR"))
+            {
+                var jsonDoc = JsonDocument.Parse(Encoding.ASCII.GetBytes(response));
+                var rate = jsonDoc.RootElement.GetProperty("bpi").GetProperty(currency).GetProperty("rate");
+
+                return Double.Parse(rate.GetString());
+            }
+
+            return 0;
+        }
+
+        public async Task<double> ConvertBitcoins(string currency, int coins)
+        {
+            var dollars = await GetExchangeRate(currency) * coins;
+            return dollars;
+        }
+
+    }
+}
---
+++ BitcoinConverter.Tests/BitcoinConverterSvcShould.cs
@@ -0,0 +1,65 @@
+using Xunit;
+
+namespace CloudAcademy.Bitcoin.Tests
+{
+    public class BitcoinConverterSvcShould
+    {
+        private ConverterSvc converterSvc;
+
+        public BitcoinConverterSvcShould()
+        {
+            converterSvc = new ConverterSvc();
+        }
+
+        [Fact]
+        public async void GetExchangeRate_USD_ReturnsUSDExchangeRate()
+        {
+            //act
+            var exchangeRate = await converterSvc.GetExchangeRate("USD");
+
+            //assert
+            double expected = 100.00;
+            Assert.Equal(expected, exchangeRate);
+        }
+
+        [Fact]
+        public async void GetExchangeRate_GBP_ReturnsGBPExchangeRate()
+        {
+            //act
+            var exchangeRate = await converterSvc.GetExchangeRate("GBP");
+
+            //assert
+            double expected = 200.00;
+            Assert.Equal(expected, exchangeRate);
+        }
+
+        [Fact]
+        public async void GetExchangeRate_EUR_ReturnsEURExchangeRate()
+        {
+            //act
+            var exchangeRate = await converterSvc.GetExchangeRate("EUR");
+
+            //assert
+            double expected = 300.00;
+            Assert.Equal(expected, exchangeRate);
+        }
+
+        [Theory]
+        [InlineData("USD",1,100.00)]
+        [InlineData("USD",2,200.00)]
+        [InlineData("GBP",1,200.00)]
+        [InlineData("GBP",2,400.00)]
+        [InlineData("EUR",1,300.00)]
+        [InlineData("EUR",2,600.00)]
+        public async void ConvertBitcoins_BitCoinsToCurrency_ReturnsCurrency(string currency, int coins, double convertedDollars)
+        {
+            //act
+            var coverterSvc = new ConverterSvc();
+            var dollars = await converterSvc.ConvertBitcoins(currency, coins);
+
+            //assert
+            var expected = convertedDollars;
+            Assert.Equal(expected, dollars);
+        }
+    }
+}
